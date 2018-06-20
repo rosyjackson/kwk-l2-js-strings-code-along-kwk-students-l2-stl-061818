0d7d9c9ff640dd2a10f2c9eef0afc6cebc0e38dc
rosyjackson <rosyjackson2004@gmail.com>
2018-06-20 21:05:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Write your code in this file!
 
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+const firstInitial = currentUser[0];
+
+const restOfName = currentUser.slice(1);
+
