e821a072613eea767dd73c95a3125452c814ef78
rosyjackson <rosyjackson2004@gmail.com>
2018-06-20 21:03:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = "Welcome, E! , ${firstInitial}, ${restOfName}";
+const shortGreeting = `Welcome, E! , ${firstInitial}, ${restOfName}`;
 
 const firstInitial = currentUser[0];
 
