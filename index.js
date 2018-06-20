f9e43a3e22adf0206862276bbe8df881c6d35a96
rosyjackson <rosyjackson2004@gmail.com>
2018-06-20 20:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const currentUser = 'Edsger Dijkstra';
- 
-const shortGreeting = ... ;
- 
-shortGreeting;
