d61791d0ea741e909c8e42d2e5dfb9f8cb0c5336
rosyjackson <rosyjackson2004@gmail.com>
2018-06-20 20:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 const currentUser = 'Grace Hopper';
 
 const welcomeMessage = 'Welcome to Klossybook, ';
+
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+ 
+"const currentUser = 'Grace Hopper';".length;
+//=> 35
+ 
+currentUser;
+//=> Uncaught ReferenceError: currentUser is not defined
